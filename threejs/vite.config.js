b382e10c81c830b6ec25a7919fb879e89327e7af
Alexandre <136007010+alexandre-bry@users.noreply.github.com>
2025-10-04 16:19:21 +0200

    Put back all HTML files in build

--- threejs/vite.config.js
+++ threejs/vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-    // build: {
-    //     rollupOptions: {
-    //         input: {
-    //             main: resolve(__dirname, 'index.html'),
-    //             about: resolve(__dirname, 'about.html'),
-    //             feedback: resolve(__dirname, 'feedback.html'),
-    //         }
-    //     }
-    // },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                about: resolve(__dirname, 'about.html'),
+                feedback: resolve(__dirname, 'feedback.html'),
+            }
+        }
+    },
     server: {
         // Vite runs on its own port (default 5173)
         // Forward all /api/* requests to the Express backend
