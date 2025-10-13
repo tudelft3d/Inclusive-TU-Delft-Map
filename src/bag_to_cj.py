Fixed units

src/bag_to_cj.py
@@ -13,10 +13,15 @@ from cj_loader import CityjsonLoader, cj_object_to_mesh
 from cj_objects import (
     Building,
     BuildingPart,
+    BuildingUnit,
+    BuildingUnitContainer,
     CityJSONFile,
     CityJSONObject,
     CityJSONObjectSubclass,
+    CityJSONSpace,
+    CityJSONSpaceSubclass,
 )
+from csv_utils import csv_format_type, csv_get_row_value, csv_read_attributes
 from geometry_utils import merge_trimeshes, orient_polygons_z_up
 
 
@@ -109,20 +114,6 @@ class Bag2Cityjson(CityjsonLoader):
         skip_column: Optional[str],
         parent_column: Optional[str],
     ) -> CityJSONFile:
-        if bdgs_attr_path is None and bdgs_sub_attr_path is not None:
-            raise ValueError(
-                "When attributes are given for the buildings subdivisions, attributes must be given for the buildings."
-            )
-        if bdgs_attr_path is not None:
-            if bag_column is None or id_column is None or skip_column is None:
-                raise ValueError(
-                    "When there are attributes to attach to the objects, 'bag_column', 'id_column' and 'skip_column' should be specified."
-                )
-        if bdgs_sub_attr_path is not None:
-            if parent_column is None:
-                raise ValueError(
-                    "When there are attributes to attach to the buildings subdivisions, 'parent_column' should be specified."
-                )
 
         # Used to remember which 3D BAG buildings have already been processed
         unprocessed_bag_ids: set[str] = set(self.data["CityObjects"].keys())
@@ -164,68 +155,58 @@ class Bag2Cityjson(CityjsonLoader):
 
         # Load the buildings attributes
         if bdgs_attr_path is None:
-            bdgs_reader = []
+            bdgs_attributes_all, bdgs_specific_values_all = [], []
         else:
-            with open(bdgs_attr_path, "r", encoding="utf-8-sig") as csvfile:
-                bdgs_reader = list(csv.DictReader(csvfile, delimiter=";"))
-            # Check that the columns are valid
-            if len(bdgs_reader) > 0:
-                row0 = bdgs_reader[0]
-                for col in [bag_column, id_column, skip_column]:
-                    if bag_column not in row0.keys():
-                        raise ValueError(
-                            f"The column {col} is not in the buildings attributes."
-                        )
+            if bag_column is None:
+                raise ValueError(
+                    f"The `bag_column` must be specified if attributes for buildings are given."
+                )
+            if id_column is None:
+                raise ValueError(
+                    f"The `id_column` must be specified if attributes for buildings are given."
+                )
+            if skip_column is None:
+                raise ValueError(
+                    f"The `skip_column` must be specified if attributes for buildings are given."
+                )
+            bdgs_specific_columns = (bag_column, id_column, skip_column)
+            bdgs_attributes_all, bdgs_specific_values_all = csv_read_attributes(
+                csv_path=bdgs_attr_path, specific_columns=bdgs_specific_columns
+            )
 
         # Load the buildings subdivisions attributes
         if bdgs_sub_attr_path is None:
-            bdgs_sub_reader = []
+            subs_attributes_all, subs_specific_values_all = [], []
         else:
-            with open(bdgs_sub_attr_path, encoding="utf-8-sig") as csvfile:
-                bdgs_sub_reader = list(csv.DictReader(csvfile, delimiter=";"))
-            # Check that the columns are valid
-            if len(bdgs_sub_reader) > 0:
-                row0 = bdgs_sub_reader[0]
-                for col in [bag_column, id_column, skip_column, parent_column]:
-                    if bag_column not in row0.keys():
-                        raise ValueError(
-                            f"The column {col} is not in the buildings attributes."
-                        )
+            if parent_column is None:
+                raise ValueError(
+                    f"The `parent_column` must be specified if attributes for buildings are given."
+                )
+            if id_column is None:
+                raise ValueError(
+                    f"The `id_column` must be specified if attributes for buildings are given."
+                )
+            if skip_column is None:
+                raise ValueError(
+                    f"The `skip_column` must be specified if attributes for buildings are given."
+                )
+            subs_specific_columns = (parent_column, id_column, skip_column)
+            subs_attributes_all, subs_specific_values_all = csv_read_attributes(
+                csv_path=bdgs_sub_attr_path, specific_columns=subs_specific_columns
+            )
 
         # Iterate over the buildings
-        for row in tqdm(bdgs_reader, desc="Processing the buildings with attributes"):
-            # Get the object id
-            obj_id = row[id_column]
-
-            # Extract the ids of all the BAG buildings that constitue this building
-            all_bag_ids = row[bag_column].split(",") if row[bag_column] != "" else []
-
-            skip = row[skip_column] != ""
-
-            geoms = _process_bag_element(
-                obj_id=obj_id,
-                bag_ids=all_bag_ids,
-                unprocessed_bag_ids=unprocessed_bag_ids,
-                skip=skip,
-            )
-
-            if geoms is not None:
-                obj_id_hyphens = obj_id.replace(".", "-")
-                building = Building(object_id=obj_id_hyphens, geometries=geoms)
-                building.add_attributes(row)
-                all_objects_cj[obj_id_hyphens] = building
-
-        for row in tqdm(
-            bdgs_sub_reader, desc="Processing the sub-buildings with attributes"
+        for attributes, specific_values in tqdm(
+            zip(bdgs_attributes_all, bdgs_specific_values_all),
+            desc="Processing the buildings with attributes",
+            total=len(bdgs_attributes_all),
         ):
-            # Get the object and parent id
-            parent_id = row[parent_column]
-            obj_id = parent_id + "." + row[id_column]
+            # Extract the specific columns
+            _, all_bag_ids = specific_values[0]
+            _, space_id = specific_values[1]
+            _, skip = specific_values[2]
 
-            # Extract the ids of all the BAG buidlings that constitue this building
-            all_bag_ids = row[bag_column].split(",") if row[bag_column] != "" else []
-
-            skip = row[skip_column] != ""
+            obj_id = Building.space_number_to_id(number=space_id)
 
             geoms = _process_bag_element(
                 obj_id=obj_id,
@@ -235,14 +216,64 @@ class Bag2Cityjson(CityjsonLoader):
             )
 
             if geoms is not None:
-                obj_id_hyphens = obj_id.replace(".", "-")
-                subdivision = BuildingPart(object_id=obj_id_hyphens, geometries=geoms)
-                subdivision.add_attributes(row)
-                all_objects_cj[obj_id_hyphens] = subdivision
-                # Connect to the parent Building
-                parent_id_hyphens = parent_id.replace(".", "-")
-                parent_obj = all_objects_cj[parent_id_hyphens]
-                CityJSONObject.add_parent_child(parent=parent_obj, child=subdivision)
+                building = Building(
+                    object_id=obj_id, space_id=space_id, geometries=geoms
+                )
+                building.add_attributes(attributes)
+                all_objects_cj[obj_id] = building
+
+        # Iterate over the units
+        for attributes, specific_values in tqdm(
+            zip(subs_attributes_all, subs_specific_values_all),
+            desc="Processing the units",
+            total=len(subs_attributes_all),
+        ):
+            # Extract the specific columns
+            _, parent_space_id = specific_values[0]
+            _, unit_id = specific_values[1]
+            _, skip = specific_values[2]
+            prefix = CityJSONSpace.space_number_to_prefix(number=parent_space_id)
+
+            # Add the missing hierarchy in the codes
+            main_container_id = BuildingUnitContainer.unit_code_to_id(
+                code="", prefix=prefix
+            )
+            if main_container_id not in all_objects_cj:
+                main_container = BuildingUnitContainer(
+                    object_id=main_container_id, unit_code=""
+                )
+                bdg_obj_id = Building.space_number_to_id(number=parent_space_id)
+                bdg_obj = all_objects_cj[bdg_obj_id]
+                all_objects_cj[main_container_id] = main_container
+                CityJSONObject.add_parent_child(parent=bdg_obj, child=main_container)
+
+            z_container_id = BuildingUnitContainer.unit_code_to_id(
+                code="Z", prefix=prefix
+            )
+            if z_container_id not in all_objects_cj:
+                z_container = BuildingUnitContainer(
+                    object_id=z_container_id, unit_code="Z"
+                )
+                all_objects_cj[z_container_id] = z_container
+                CityJSONObject.add_parent_child(
+                    parent=main_container, child=z_container
+                )
+            z_container = all_objects_cj[z_container_id]
+            assert isinstance(z_container, BuildingUnitContainer)
+
+            # Find the number of units with the same code
+            units_same_code = len(z_container.children_ids)
+
+            obj_id = BuildingUnit.unit_code_to_id(
+                code="Z", prefix=prefix, number=units_same_code
+            )
+            unit = BuildingUnit(object_id=obj_id, unit_code="Z")
+            unit.add_attributes({"subdivision_number": unit_id})
+            unit.add_attributes(attributes)
+            all_objects_cj[obj_id] = unit
+
+            # Connect to the parent building
+            CityJSONObject.add_parent_child(parent=z_container, child=unit)
 
         # Add the remaining buildings
         for bag_2d_id in tqdm(
@@ -252,15 +283,19 @@ class Bag2Cityjson(CityjsonLoader):
             if bag_2d_id[-2] == "-":
                 continue
 
+            obj_id = Building.space_number_to_id(number=bag_2d_id)
+
             geoms = _process_bag_element(
-                obj_id=bag_2d_id,
+                obj_id=obj_id,
                 bag_ids=[bag_2d_id],
             )
 
             if geoms is not None:
-                bag_2d_id_hyphens = bag_2d_id.replace(".", "-")
-                building = Building(object_id=obj_id_hyphens, geometries=geoms)
-                all_objects_cj[bag_2d_id_hyphens] = building
+
+                building = Building(
+                    object_id=obj_id, space_id=bag_2d_id, geometries=geoms
+                )
+                all_objects_cj[obj_id] = building
 
         cj_file = CityJSONFile(
             scale=np.array([0.00001, 0.00001, 0.00001], dtype=np.float64),

src/gltf_to_cj.py
@@ -13,13 +13,16 @@ from cj_objects import (
     Building,
     BuildingPart,
     BuildingRoom,
+    BuildingRoot,
     BuildingStorey,
     BuildingUnit,
     BuildingUnitContainer,
     CityJSONFile,
     CityJSONObject,
     CityJSONObjectSubclass,
+    CityJSONSpaceSubclass,
 )
+from csv_utils import csv_format_type, csv_get_row_value, csv_read_attributes
 from geometry_utils import flatten_trimesh, orient_polygons_z_up
 
 
@@ -46,71 +49,6 @@ def _get_scene_geometry_from_id(
     return geometry
 
 
-# def extract_objects_first_level_gltf(gltf_path: Path) -> dict[str, trimesh.Trimesh]:
-#     # Load the scene
-#     scene = trimesh.load_scene(gltf_path)
-#     nodes_names: list[str] = scene.graph.nodes_geometry
-
-#     geometries = {}
-
-#     for node_name in nodes_names:
-#         geometry = get_scene_geometry_from_id(scene=scene, object_id=node_name)
-#         geometries[node_name] = geometry
-
-#     return geometries
-
-
-# def building_parts_from_gltf(gltf_path: Path, id_prefix: str) -> list[BuildingPart]:
-#     geometries_3D = extract_objects_first_level_gltf(gltf_path=gltf_path)
-#     bdg_parts: list[BuildingPart] = []
-
-#     for bdg_part_name, geometry_3D in geometries_3D.items():
-#         # Fix the 3D geometry
-#         geometry_3D.process(validate=True)
-#         geometry_3D.fill_holes()
-#         geometry_3D.fix_normals()
-
-#         # Build the geometry part
-#         bdg_part_geometry_2D = MultiSurface.from_mesh(
-#             lod=0, mesh=flatten_trimesh(geometry_3D)
-#         )
-#         bdg_part_geometry_3D = MultiSurface.from_mesh(lod=2, mesh=geometry_3D)
-
-#         # Extract all the triangles into Polygons
-#         bdg_part_id = id_prefix + bdg_part_name
-#         room = BuildingPart(
-#             object_id=bdg_part_id,
-#             attributes={"name": bdg_part_name},
-#             geometries=[bdg_part_geometry_2D, bdg_part_geometry_3D],
-#         )
-#         bdg_parts.append(room)
-
-#     return bdg_parts
-
-
-# def building_rooms_from_gltf(gltf_path: Path, id_prefix: str) -> list[BuildingRoom]:
-#     geometries = extract_objects_first_level_gltf(gltf_path=gltf_path)
-#     rooms: list[BuildingRoom] = []
-
-#     for room_name, geometry in geometries.items():
-#         # Fix the orientation
-#         orient_polygons_z_up(geometry)
-
-#         # Build the geometry part
-#         room_geometry = MultiSurface.from_mesh(lod=0, mesh=geometry)
-
-#         # Extract all the triangles into Polygons
-#         room_id = id_prefix + room_name
-#         room = BuildingRoom(
-#             object_id=room_id,
-#             attributes={"name": room_name},
-#             geometries=[room_geometry],
-#         )
-#         rooms.append(room)
-
-#     return rooms
-
-
 def _geom_and_name_from_scene_id(
     scene: trimesh.Scene, object_id: str
 ) -> tuple[Geometry, str]:
@@ -120,45 +58,6 @@ def _geom_and_name_from_scene_id(
     if lod == 0:
         orient_polygons_z_up(mesh)
     return MultiSurface.from_mesh(lod=lod, mesh=mesh), name
-
-
-def _csv_format_type(value: Any, column_type: str) -> Any:
-    if column_type == "str":
-        return str(value)
-    elif column_type == "float":
-        if value == "":
-            return None
-        return float(value.replace(",", "."))
-    elif column_type == "int":
-        if value == "":
-            return None
-        return int(value)
-    elif column_type.startswith("list"):
-        if value == "":
-            return []
-        list_info = column_type[len("list") :]
-        separator = list_info[0]
-        other_type = list_info[1:]
-        return [
-            _csv_format_type(value=v, column_type=other_type)
-            for v in value.split(separator)
-        ]
-    else:
-        raise NotImplementedError(
-            f"Support for type '{column_type}' is not implemented yet."
-        )
-
-
-def _csv_get_row_value(row: dict[str, Any], column: str) -> tuple[str, Any]:
-    value = row[column]
-    column_split = column.split(" [")
-    if len(column_split) != 2:
-        raise RuntimeError(
-            f"The column name should look like this: '<Name> [<type>]', but it is '{column}'."
-        )
-    column_type = column_split[1][:-1]
-    column_name = column_split[0]
-    return column_name, _csv_format_type(value=value, column_type=column_type)
 
 
 def _unit_code_to_parent(code: str) -> str:
@@ -172,41 +71,17 @@ def _unit_code_to_parent(code: str) -> str:
         return code[:-2]
 
 
-def _unit_code_to_id(code: str, prefix: str) -> str:
-    return f"{BuildingUnitContainer.type_name}-{prefix}-{code}"
-
-
 def load_attributes_from_csv(
     csv_path: Path, id_column: str
 ) -> dict[str, dict[str, Any]]:
-    attributes = {}
-    with open(csv_path, encoding="utf-8-sig") as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=";")
-        for row in reader:
-            # Skip empty rows
-            if not any(cell != "" for cell in row.values()):
-                continue
-            # Process the row
-            col_name, id_value = _csv_get_row_value(row=row, column=id_column)
-            if id_value in attributes:
-                raise RuntimeError(
-                    f"Column {id_column} cannot be they key, because multiple rows share the same value (e.g. {id_value})."
-                )
-            row_attributes = {}
-            for col_name_type in row.keys():
-                # Skip columns that don't have a type
-                if col_name_type.find(" [") == -1:
-                    continue
-                # Add the column and its value to the attributes
-                col_name, col_value = _csv_get_row_value(row=row, column=col_name_type)
-                if col_name in row_attributes:
-                    raise RuntimeError(
-                        f"Two columns have the same name '{col_name}' in {str(csv_path)}"
-                    )
-                row_attributes[col_name] = col_value
-
-            attributes[id_value] = row_attributes
-    return attributes
+    attributes_all, specific_values_all = csv_read_attributes(
+        csv_path=csv_path, specific_columns=(id_column,)
+    )
+    id_to_attributes: dict[str, dict[str, Any]] = {}
+    for i in range(len(attributes_all)):
+        _, id_value = specific_values_all[i][0]
+        id_to_attributes[id_value] = attributes_all[i]
+    return id_to_attributes
 
 
 def load_units_from_csv(
@@ -216,52 +91,43 @@ def load_units_from_csv(
     spaces_column: str,
 ) -> None:
     root_pos = cj_file.get_root_position()
-    prefix_ids = cj_file.city_objects[root_pos].id.replace("-", "_")
+    root = cj_file.city_objects[root_pos]
+    if not isinstance(root, Building):
+        raise RuntimeError(
+            f"The root of the `cj_file` should be a Building, not a {type(root)}"
+        )
+    prefix = Building.space_number_to_id(number=root.space_id)
 
     all_units: dict[str, list[BuildingUnit]] = defaultdict(lambda: [])
 
     # Process the CSV file to find all the units
-    with open(csv_path, encoding="utf-8-sig") as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=";")
-        for row in reader:
-            # Skip empty rows
-            if not any(cell != "" for cell in row.values()):
-                continue
-            # Process the row
-            code_column_name, code_value = _csv_get_row_value(
-                row=row, column=code_column
-            )
+    unit_to_spaces: dict[str, list[str]] = {}
+    specific_columns = (code_column, spaces_column)
+    attributes_all, specific_values_all = csv_read_attributes(
+        csv_path=csv_path, specific_columns=specific_columns
+    )
+    for attributes, specific_values in zip(attributes_all, specific_values_all):
+        _, unit_code = specific_values[0]
+        _, unit_spaces = specific_values[1]
 
-            unit_container_id = _unit_code_to_id(code=code_value, prefix=prefix_ids)
-            unit_id = f"{unit_container_id}@{len(all_units[code_value])}"
+        current_units_same_code = len(all_units[unit_code])
+        unit_id = BuildingUnit.unit_code_to_id(
+            code=unit_code, prefix=prefix, number=current_units_same_code
+        )
 
-            # Load the columns that contain a type as attributes
-            attributes = {}
-            for col_name_type in row.keys():
-                # Skip columns that don't have a type
-                if col_name_type.find(" [") == -1:
-                    continue
-                # Add the column and its value to the attributes
-                col_name, col_value = _csv_get_row_value(row=row, column=col_name_type)
-                # Rename the columns used specifically
-                if col_name_type == spaces_column:
-                    col_name = BuildingUnit.unit_children
-                elif col_name_type == code_column:
-                    col_name = CityJSONObject.unit_code
-                if col_name in attributes:
-                    raise RuntimeError(
-                        f"Two columns have the same name '{col_name}' in {str(csv_path)}"
-                    )
-                attributes[col_name] = col_value
-
-            bdg_unit = BuildingUnit(object_id=unit_id, attributes=attributes)
-            all_units[code_value].append(bdg_unit)
+        unit = BuildingUnit(
+            object_id=unit_id,
+            unit_code=unit_code,
+            attributes=attributes,
+        )
+        unit_to_spaces[unit.id] = unit_spaces
+        all_units[unit_code].append(unit)
 
     # Add the missing hierarchy in the codes
+    main_container_id = BuildingUnitContainer.unit_code_to_id(code="", prefix=prefix)
     all_unit_containers: dict[str, BuildingUnitContainer] = {
         BuildingUnitContainer.main_parent: BuildingUnitContainer(
-            object_id=f"{BuildingUnitContainer.main_parent}-{prefix_ids}",
-            attributes={CityJSONObject.unit_code: ""},
+            object_id=main_container_id, unit_code=""
         )
     }
     current_codes = list(all_units.keys())
@@ -270,17 +136,18 @@ def load_units_from_csv(
             if not code in all_units.keys():
                 all_units[code] = []
             if not code in all_unit_containers.keys():
-                obj_id = _unit_code_to_id(code=code, prefix=prefix_ids)
+                obj_id = BuildingUnitContainer.unit_code_to_id(code=code, prefix=prefix)
                 all_unit_containers[code] = BuildingUnitContainer(
-                    object_id=obj_id, attributes={CityJSONObject.unit_code: code}
+                    object_id=obj_id, unit_code=code
                 )
             code = _unit_code_to_parent(code=code)
 
     # Extract all the spaces from the given CityJSON file
     spaces_ids_to_pos = {}
     for i, cj_obj in enumerate(cj_file.city_objects):
-        space_id = cj_obj.attributes[CityJSONObject.space_id]
-        spaces_ids_to_pos[space_id] = i
+        # We seach for the actual spaces
+        if isinstance(cj_obj, CityJSONSpaceSubclass):
+            spaces_ids_to_pos[cj_obj.space_id] = i
 
     # Apply the parent-child relationships of unit containers
     for code, unit_container in all_unit_containers.items():
@@ -302,18 +169,11 @@ def load_units_from_csv(
     # Add the links from spaces to the units they belong in
     all_units_flattened = [unit for units in all_units.values() for unit in units]
     for unit in all_units_flattened:
-        for space_id in unit.attributes[BuildingUnit.unit_children]:
+        for space_id in unit_to_spaces[unit.id]:
             cj_file_pos = spaces_ids_to_pos[space_id]
-            if (
-                BuildingUnit.space_parents
-                not in cj_file.city_objects[cj_file_pos].attributes
-            ):
-                cj_file.city_objects[cj_file_pos].add_attributes(
-                    {BuildingUnit.space_parents: []}
-                )
-            cj_file.city_objects[cj_file_pos].attributes[
-                BuildingUnit.space_parents
-            ].append(unit.id)
+            CityJSONObject.add_unit_space(
+                unit=unit, space=cj_file.city_objects[cj_file_pos]
+            )
 
     cj_file.add_cityjson_objects(all_unit_containers.values())
     cj_file.add_cityjson_objects(
@@ -366,31 +226,23 @@ def full_building_from_gltf(gltf_path: Path) -> CityJSONFile:
         hierarchy_level = name.count(".")
         if hierarchy_level == 0:
             obj_func = Building
-            obj_prefix = "Building-"
         elif hierarchy_level == 1:
             obj_func = BuildingPart
-            obj_prefix = "BuildingPart-"
         elif hierarchy_level == 2:
             obj_func = BuildingStorey
-            obj_prefix = "BuildingStorey-"
         elif hierarchy_level == 3:
             obj_func = BuildingRoom
-            obj_prefix = "BuildingRoom-"
         else:
             raise RuntimeError(f"Unexpected format for an object name: '{name}'")
 
-        # Replace the dots in the name by hyphens
-        obj_id = name.replace(".", "-")
-
-        # Add the type to the object key
-        obj_id = f"{obj_prefix}{obj_id}"
-
-        # Store the initial key as an attribute
-        attributes = {CityJSONObject.space_id: name}
+        obj_id = obj_func.space_number_to_id(number=name)
 
         all_objects_cj[name] = obj_func(
-            object_id=obj_id, geometries=geoms, attributes=attributes
+            object_id=obj_id, space_id=name, geometries=geoms
         )
+
+    # # Add the root group
+    # root = BuildingRoot(object_id=f"Root-08")
 
     # Apply the parent-child relationships
     for obj_name in all_objects_cj.keys():
@@ -400,11 +252,14 @@ def full_building_from_gltf(gltf_path: Path) -> CityJSONFile:
             CityJSONObject.add_parent_child(
                 parent=all_objects_cj[obj_parent_name], child=all_objects_cj[obj_name]
             )
+        # else:
+        #     CityJSONObject.add_parent_child(parent=root, child=all_objects_cj[obj_name])
 
     cj_file = CityJSONFile(
         scale=np.array([0.00001, 0.00001, 0.00001], dtype=np.float64),
         translate=np.array([0, 0, 0], dtype=np.float64),
     )
+    # cj_file.add_cityjson_objects([root])
     cj_file.add_cityjson_objects(all_objects_cj.values())
 
     return cj_file