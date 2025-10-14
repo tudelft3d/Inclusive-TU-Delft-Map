43326ff9c8ba9dbbeea52c90bdc8a942b3e04bf2
DaanSchlosser <daan.schlosser@gmail.com>
2025-10-14 20:54:19 +0200

    Added default home position which is right above, and oriented in line with TU Delft

--- threejs/src/app.js
+++ threejs/src/app.js
@@ -475,8 +475,15 @@ export class Map {
             //     child.visible = true;
             // } else { child.visible = false; }
             // });
-            this.cameraManager.camera.position.set(center.x, center.y + maxDim * 0.5, center.z + cameraZ);
-            this.cameraManager.controls.target.copy(center);
+
+            // Old camera positioning based on model bounds
+            // this.cameraManager.camera.position.set(center.x, center.y + maxDim * 0.5, center.z + cameraZ);
+            // this.cameraManager.controls.target.copy(center);
+
+            // New standard view position and target
+            this.cameraManager.camera.position.set(85715.53268458637, 1099.5279016009758, -445779.7690020757);
+            this.cameraManager.controls.target.set(85743.30835529274, 43.249941349128534, -445791.2428672409);
+
             this.cameraManager.controls.update();
             this.cameraManager.setHomeView();
 
