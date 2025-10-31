040d8334872968f75f986babd917b8bc4eb8f970
Alexandre <136007010+alexandre-bry@users.noreply.github.com>
2025-10-31 17:16:09 +0100

    Fix #123

--- threejs/src/app.js
+++ threejs/src/app.js
@@ -40,8 +40,8 @@ export class Map {
         this.preloadingStarted = false; // Flag to ensure preloading starts only once
 
         // Cameras and controls
-        const cameraPosition = new THREE.Vector3(85715, 1100, -445780);
-        const cameraLookAt = new THREE.Vector3(85743, 30, -445791);
+        const cameraPosition = new THREE.Vector3(85070, 942, -445825);
+        const cameraLookAt = new THREE.Vector3(85478, 0, -446006);
         this.cameraManager = new CamerasControls(
             this.mainContainer,
             cameraPosition,
@@ -51,6 +51,7 @@ export class Map {
         this.tweens = new Array();
 
         this._initScenes();
+        this.cjHelper = new CjHelper(this.scene);
         this._initLights();
         this.setBasemap();
         this._initRenderers();
