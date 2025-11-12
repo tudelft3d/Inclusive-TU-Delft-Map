dbef525e2c0f3207b33109cd01fbe32c038baf9a
Alexandre <136007010+alexandre-bry@users.noreply.github.com>
2025-11-12 16:12:46 +0100

    Fix the tutorial not appearing on the server version

--- threejs/vite.config.js
+++ threejs/vite.config.js
@@ -8,16 +8,18 @@ export default defineConfig({
                 main: resolve(__dirname, 'index.html'),
                 about: resolve(__dirname, 'about.html'),
                 feedback: resolve(__dirname, 'feedback.html'),
+                tutorial: resolve(__dirname, 'tutorial.html'),
                 404: resolve(__dirname, '404.html'),
             }
         }
     },
     server: {
         // Vite runs on its own port (default 5173)
-        // Forward all /api/* requests to the Express backend
+        // Forward all /api/* requests to the feedback handler
+        // This is necessary to have the feedback properly working
         proxy: {
             '/api': {
-                target: 'http://localhost:3000', // same port as the Express app
+                target: 'http://localhost:3000', // same port as the feedback app
                 changeOrigin: true,
                 secure: false,
             },
